0a00328b0eb359872e9951fb8aa601ed6861dcfa
Félix Bouveret <bouveret.felix@gmail.com>
2021-01-01 21:12:54 +0100

    Add core game

--- src/modules/MineSweeper/MineSweeper.vue
+++ src/modules/MineSweeper/MineSweeper.vue
@@ -10,10 +10,14 @@
             :class="[
               { isShowed: j.isShowed },
               { isFlaged: j.isFlaged },
-              { hasBomb: j.hasBomb },
+              { hasBomb: j.hasBomb && j.isShowed },
             ]"
             @click="blockClick($event, i, j)"
-          />
+          >
+            <span v-if="showBlockNumber(j)">
+              {{ showBlockNumber(j) }}
+            </span>
+          </div>
         </div>
       </div>
     </div>
@@ -24,9 +28,12 @@
 export default {
   data() {
     return {
-      boardSize: 30,
+      boardSize: 15,
       boardGrid: [],
       bombList: [],
+      hasClicked: false,
+      showLoosePopup: false,
+      showWinPopup: false,
     }
   },
 
@@ -70,24 +77,44 @@ export default {
             isShowed: false,
             isFlaged: false,
             hasBomb: false,
+            bombNumber: 0,
+            isFirstClick: false,
           })
         }
         this.boardGrid.push(row)
       }
-      this.createBombs()
     },
 
     blockClick(event, rowObject, colObject) {
+      if (!this.hasClicked) {
+        this.firstClick(colObject)
+      } else {
+        this.regularClick(event, rowObject, colObject)
+      }
+    },
+
+    firstClick(colObject) {
+      colObject.isFirstClick = true
+      this.hasClicked = true
+      this.createBombs()
+      colObject.isShowed = true
+    },
+
+    regularClick(event, rowObject, colObject) {
       if (colObject.isShowed) return
       if (event.shiftKey) {
-        if (colObject.isFlaged) {
-          this.unflagBlock(rowObject, colObject)
-        } else {
-          this.flagBlock(rowObject, colObject)
-        }
+        this.flagClick(rowObject, colObject)
       } else {
         if (colObject.isFlaged) return
-        this.showBlock(rowObject, colObject)
+        if (colObject.hasBomb) this.showBlock(rowObject, colObject)
+      }
+    },
+
+    flagClick(rowObject, colObject) {
+      if (colObject.isFlaged) {
+        this.unflagBlock(rowObject, colObject)
+      } else {
+        this.flagBlock(rowObject, colObject)
       }
     },
 
@@ -104,14 +131,45 @@ export default {
       console.log('show', colObject.isShowed)
     },
 
+    setBombNumber() {
+      this.flatGrid.forEach((block) => {
+        this.searchBlockForBombs(block)
+      })
+    },
+
+    searchBlockForBombs(colObject) {
+      if (colObject.hasBomb) return
+      const colCoordonates = {
+        row: parseInt(colObject.gId.split('-')[0]),
+        col: parseInt(colObject.gId.split('-')[1]),
+      }
+      let bombNumber = 0
+      for (let row = -1; row < 2; row++) {
+        for (let col = -1; col < 2; col++) {
+          const gId = `${colCoordonates.row + row}-${colCoordonates.col + col}`
+          const element = this.flatGrid.find((e) => e.gId === gId)
+
+          if (element?.hasBomb) {
+            bombNumber++
+          }
+        }
+      }
+
+      colObject.bombNumber = bombNumber
+    },
+
     createBombs() {
       for (let bomb = 0; bomb < this.bombNumber; bomb++) {
         this.setRandomBomb()
       }
+      this.setBombNumber()
     },
 
     setRandomBomb() {
-      if (this.flatGrid[this.getRandomBombId()].hasBomb) {
+      if (
+        this.flatGrid[this.getRandomBombId()].hasBomb &&
+        this.flatGrid[this.getRandomBombId()].isFirstClick
+      ) {
         this.setRandomBomb()
       } else {
         const randomBlock = this.flatGrid[this.getRandomBombId()]
@@ -123,6 +181,29 @@ export default {
     getRandomBombId() {
       return Math.floor(Math.random() * this.totalBlocks)
     },
+
+    showBlockNumber(colObject) {
+      if (colObject.isShowed && colObject.bombNumber)
+        return colObject.bombNumber
+      return ''
+    },
+
+    gameEnd(status) {
+      switch (status) {
+        case 'loose':
+          this.showLoosePopup = true
+          break
+
+        case 'win':
+          this.showWinPopup = true
+          break
+
+        default:
+          break
+      }
+    },
+
+    loose() {},
   },
 }
 </script>
@@ -145,9 +226,25 @@ export default {
 }
 
 .mine_block {
+  position: relative;
+
   width: 100%;
   height: 100%;
   border: solid 1px #00000050;
+
+  color: black;
+
+  cursor: default;
+
+  user-select: none;
+
+  span {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    transform: translate(-50%, -50%);
+  }
 
   &:hover {
     background-color: #00000050;
